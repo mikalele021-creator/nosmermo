b49fda208416b400b952f6156993dd56974c4829
mikalele021-creator <mikalele021@gmail.com>
2026-03-17 10:28:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+//const nome = "leticia"
+
+//console.log(nome)
+
+
+let userName = " Mikaely"
+let email = "Mikaely@gmail.com"
+console.log (userName, email, "teste")
+let message = "Olá," + userName + "Você conectou com o email " +  email 
+console.log(message)
+console.log(`Olá, ${userName} Você conectou com o email ${email}`)
