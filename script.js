417d3873aaca60da69d232e01f65f352a867146d
mikalele021-creator <mikalele021@gmail.com>
2026-03-20 08:37:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,9 +3,43 @@
 //console.log(nome)
 
 
-let userName = " Mikaely"
-let email = "Mikaely@gmail.com"
-console.log (userName, email, "teste")
-let message = "Olá," + userName + "Você conectou com o email " +  email 
-console.log(message)
-console.log(`Olá, ${userName} Você conectou com o email ${email}`)
+//let userName = " Mikaely"
+//let email = "Mikaely@gmail.com"
+//console.log (userName, email, "teste")
+//let message = "Olá," + userName + "Você conectou com o email " +  email 
+//console.log(message)
+//console.log(`Olá, ${userName} Você conectou com o email ${email}`)
+ //let number = 30 
+ //number+=100
+// console.log(number)
+
+//AND &&
+//OR ||
+//NOT !
+
+//let email = true 
+//let password = false 
+
+//console.log(email && password)
+//console.log(email || password)
+//console.log(!email)
+
+//let idade = 17
+//let temIngresso = false
+
+//if (idade >= 20 || temIngresso) {
+  //  console.log("Pode entrar no evento!");
+//} else {
+ //   console.log(" Não pode entrar.");
+//}
+
+let horas = 12
+
+if(horas <= 12){
+    console.log("Bom dia!")
+
+}else if(horas > 12 && horas<= 18){
+    console.log( "Boa tarde!")
+} else{
+    console.log( " Boa Noite!")
+}
