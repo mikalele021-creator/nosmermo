241304b1defcc62697541768e3f806015ea130ac
mikalele021-creator <mikalele021@gmail.com>
2026-03-26 00:30:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -44,12 +44,69 @@
    // console.log( " Boa Noite!")
 //}
 
-let numberOne = 14
-let numberTwo = 2
-let resto = numberOne % numberTwo
 
-if(resto == 0){
-    console.log( "par")
-}else{ 
-    console("impar")
+//PRIMEIRA
+
+
+//let numberOne = 14
+//let numberTwo = 2
+//let resto = numberOne % numberTwo
+
+//if(resto == 0){
+  //  console.log( "par")
+//}else{ 
+  //  console("impar")
+//}
+
+
+//SEGUNDA
+
+//let numberOne = 15
+//let numberTwo = 22
+//if (numberOne > numberTwo){
+ // console.log ("maior número:", numberOne);
+//} else if (numberTwo > numberOne){
+//console.log ("maior número:", numberTwo);
+//}else {
+  //console.log ("os números são iguais");
+//}
+
+
+//TERCEIRA
+
+//for(let i = 1 ; i <= 10; i++){
+  //console.log( `5 x ${i}= ${5 * i}`);
+//}
+
+
+//QUARTA
+
+//let num= 10
+//let fatorial = 1;
+//for (let i = num; i > 1; i--) {
+  //fatorial *= i;
+//}
+//console.log("fatorial:", fatorial);
+
+
+//QUINTA
+
+let n1 = 8
+let n2 = 9
+let  n3 = 10
+let media = (n1 + n2 + n3) / 3;
+console.log ("média:", media);
+if (media >= 7){
+  console.log("aprovado");
+}else if (media >= 5){
+  console.log(recuperação);
+}else{
+  console.log("reprovado");
 }
+
+//SEXTA 
+
+
+//for (let i = 10; i >= 0; i--) {
+  //console.log(i);
+//}
